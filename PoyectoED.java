05e99a30aa347be3853f78b2f30532a203d9ead9
Eliefaya <efayad57@gmail.com>
2024-02-16 20:48:49 -0600

    Agregando archivos del proyecto de Java

---
+++ PoyectoED.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poyecto.ed;
+
+/**
+ *
+ * @author efaya
+ */
+public class PoyectoED {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
