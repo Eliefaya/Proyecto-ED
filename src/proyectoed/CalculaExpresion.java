0effb92becba8a498f87dcfb81a4d09d39af65f9
kfloresa <160078530+kfloresa@users.noreply.github.com>
2024-02-23 13:05:43 -0600

    Creo javadoc de clase CalculaExpresion

--- src/proyectoed/CalculaExpresion.java
+++ src/proyectoed/CalculaExpresion.java
@@ -2,11 +2,23 @@ package proyectoed;
 
 import java.util.ArrayList;
 /**
- *
+ * <pre>
+ * Clase para calcular el resultado de una operacion escrita en notacion postfija.
  * @author Kyrie
  */
 public class CalculaExpresion {
     
+    /**
+     * Calcula el resultado de una operacion escrita en notacion postfija.
+     * @param expresionPostfija (ArrayList de doubles y caracteres, estos siendo +, -, *, / y ^)
+     * @see Calculadora
+     * btIgualActionPerformed
+     * @return <ul>
+     * <li> El resultado, si puede ser calculado (double) </li>
+     * <li> Infinity o NaN </li>
+     * <li> RuntimeException "Error al realizar la operacion" si la expresion postfija tiene un error, o no se puede calcular el resultado por otra razon </li>
+     * </ul>
+     */
     public static double calculaResultado(ArrayList expresionPostfija) {
         PilaA<Double> operandos = new PilaA<Double>();
         double numero1, numero2;
